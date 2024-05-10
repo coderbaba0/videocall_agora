3f40c51ab9a84e3fd190cd3b5660d2c7264f3fa0
sattababa <sattababakingup@gmail.com>
2024-05-10 19:02:39 +0530

    initial commited on 09/05

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:agora_uikit/agora_uikit.dart';
 import 'package:flutter/material.dart';
@@ -5,10 +7,41 @@ import 'package:flutter_callkit_incoming/flutter_callkit_incoming.dart';
 import 'package:get/get.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:videocall/app/modules/splash/views/splash_view.dart';
 
 class HomeController extends GetxController {
-   var client = AgoraClient(agoraConnectionData: AgoraConnectionData(appId: '',channelName: '')).obs();
-   bool isSnackbarDisplayed = false;
+
+  RxString currentTime = '00:00'.obs;
+  Timer? _timer;
+  int _secondsElapsed = 0;
+  void startTimer() {
+    _timer?.cancel(); // Cancel previous timer if any
+    _secondsElapsed = 0;
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      _secondsElapsed++;
+      int minutes = _secondsElapsed ~/ 60;
+      int seconds = _secondsElapsed % 60;
+      currentTime.value = '${_twoDigits(minutes)}:${_twoDigits(seconds)}';
+    });
+  }
+  void stopTimer() {
+    _timer?.cancel();
+  }
+  String _twoDigits(int n) {
+    if (n >= 10) {
+      return '$n';
+    }
+    return '0$n';
+  }
+
+  var client = AgoraClient(
+          agoraConnectionData: AgoraConnectionData(appId: '', channelName: ''))
+      .obs();
+  RxBool isremoteUserConnected = false.obs;
+  RxBool isremoteUserchannelConnected = false.obs;
+  bool isSnackbarDisplayedforconnection = false;
+  bool isSnackbarDisplayedforchannel = false;
+  bool isSnackbarDisplayed = false;
   @override
   void onInit() {
     super.onInit();
@@ -21,13 +54,25 @@ class HomeController extends GetxController {
         appId: '8b8d2b1d937147288865f374f1ea815b',
         channelName: Get.arguments['channelId'],
         uid: int.parse(Get.arguments['userId']),
-        tempToken: '007eJxTYFh3r3bKstfvjx4rO7Eq4t302IteIkxlnoJ93xc9NCoyYpqjwGCRZJFilGSYYmlsbmhibmRhYWFmmmZsbpJmmJpoYWiaNDHaJq0hkJFheo4UIyMDBIL4LAwlqcUlDAwAzt4f5A==',
+        tempToken:
+            '007eJxTYLj/yFTp57w1QSnLHh/9w/LZ69+pSn+Nr3H7bvOXLNbvOLlTgcEiySLFKMkwxdLY3NDE3MjCwsLMNM3Y3CTNMDXRwtA0SXudbVpDICPD5+kVDIxQCOKzMJSkFpcwMAAAMpIiDg==',
       ),
       enabledPermission: [
         Permission.camera,
         Permission.microphone,
       ],
       agoraEventHandlers: AgoraRtcEventHandlers(
+        onConnectionStateChanged: (connection, state, reason) {
+          if (state == ConnectionStateType.connectionStateConnected) {
+            if (!isSnackbarDisplayedforconnection) {
+              //Get.snackbar('Connecting', 'Waiting for remote users to join...');
+              isremoteUserConnected.value = true;
+              isSnackbarDisplayedforconnection = true;
+            }
+          } else {
+            isSnackbarDisplayedforconnection = false;
+          }
+        },
         // onJoinChannelSuccess: (RtcConnection connection,uid){
         //   Get.snackbar('alert', '$uid+joined sucessfully');
         // },
@@ -40,18 +85,38 @@ class HomeController extends GetxController {
         //   print('muted, uid: $uid');
         //   Get.snackbar('alert', '$uid+$elapsed');
         // },
-        // onUserJoined: (connection, uid, elapsed) {
-        //   print('User joined channel, uid: $uid');
-        //   Get.snackbar('alert', '$uid+joined sucessfully');
-        //
-        // },
-
-        onUserOffline: (connection, uid, reason) async{
-          Get.back();
+        onUserJoined: (connection, uid, elapsed) {
+          if (!isSnackbarDisplayedforchannel) {
+            Get.snackbar('Message', '$uid joined sucessfully');
+            isremoteUserchannelConnected.value = true;
+            startTimer();
+            isSnackbarDisplayedforchannel = true;
+          } else {
+            isSnackbarDisplayedforchannel = false;
+          }
+        },
+        onUserOffline: (connection, uid, reason) {
+           // Get.delete<HomeController>();
+           // Get.back();
           if (!isSnackbarDisplayed) {
-            Get.snackbar('alert', '$uid left the channel');
+            Get.snackbar('Messege', '$uid left the call');
+            stopTimer();
+            Get.defaultDialog(
+                title: 'Message',
+                middleText: 'User left the call',
+                barrierDismissible: false,
+                confirm: ElevatedButton(
+                  onPressed: () {
+                    Get.delete<HomeController>(); // Dispose of the controller
+                    Get.to(()=>SplashView());
+                  },
+                  child: Text('OK'),
+                ),
+                );
             isSnackbarDisplayed = true;
-            await client.engine.leaveChannel();
+          }
+          else {
+            isSnackbarDisplayed = false;
           }
         },
         onError: (errorCode, message) {
@@ -61,28 +126,21 @@ class HomeController extends GetxController {
     );
     client.engine.leaveChannel();
   }
-   // Method to check if user is connected to the channel
-   bool isUserConnected() {
-     return ConnectionStateType.connectionStateConnected == client.engine.getConnectionState();
-   }
-   void closeConnection() async{
-    bool userconnected = isUserConnected();
-    print(userconnected.toString());
-    if(userconnected==false){
-      client.engine.leaveChannel();
-      await FlutterCallkitIncoming.endAllCalls();
-    }
-   }
+
   @override
   void onReady() async {
-   await client.initialize();
+    await client.initialize();
     super.onReady();
   }
 
   @override
-  void onClose() {
+  void onClose() async{
+    await client.engine.release();
+    await FlutterCallkitIncoming.endAllCalls();
+    _timer?.cancel();
     super.onClose();
   }
+
   void sendPushMessage() async {
     try {
       var response = await http.post(
@@ -90,22 +148,23 @@ class HomeController extends GetxController {
         headers: <String, String>{
           'Content-Type': 'application/json',
           'Authorization':
-          'key=AAAAgBOVjKQ:APA91bH8yWi9S8V5_HUY9ZsKl77acSls3zejtpZciIir16nkMpaopppXMTnEe17pgFAoPZmmcfOeL4YfiLZlAQChOgjo8X3Af8TK84DsQQtLaOWQoAneccTbFFUPjm3nZZXdcgWquzfA',
+              'key=AAAAgBOVjKQ:APA91bH8yWi9S8V5_HUY9ZsKl77acSls3zejtpZciIir16nkMpaopppXMTnEe17pgFAoPZmmcfOeL4YfiLZlAQChOgjo8X3Af8TK84DsQQtLaOWQoAneccTbFFUPjm3nZZXdcgWquzfA',
         },
         body: jsonEncode(
           <String, dynamic>{
             'priority': 'high',
             'data': <String, dynamic>{
               'click_action': 'FLUTTER_NOTIFICATION_CLICK',
-              'uid': '12',
+              'uid': '10',
               'channelid': 'test',
               'username': 'oneplus user',
               'profileimg': 'https://i.pravatar.cc/100',
               'calltype': '1',
               'timestamp': DateTime.now().toString(),
             },
-            'to': 'fCLKCUOgTrGT2hg264xqJY:APA91bFEV5LX9UIGv0k295RRb4xJirM4An0Xm7CcX3dhQmH66Pr6X34AT0hkpVRCqeLKGhYCvi9QOsZPjSl5OIMa0k7bOpG1kBB5fnj8Ukv9N2kVdtW-AzfFQPBX2lM8Zta2eve0TFH9',
-            // 'fDZEgjXDRSGLvnr3Cuxaai:APA91bGH9aLsy_vgOtKn7my0vuiOzg7ea_fMvAP8jfNBxNSM9oQMSdrr00HeFYn6XLkZl70URK9E_GdqwT9OKXkT8PQLH6J2cmWdJnxFYhx9jcatkDNf_R0S1U0CCwwSf5WLqayojL4M',
+            'to':
+                'fCLKCUOgTrGT2hg264xqJY:APA91bFEV5LX9UIGv0k295RRb4xJirM4An0Xm7CcX3dhQmH66Pr6X34AT0hkpVRCqeLKGhYCvi9QOsZPjSl5OIMa0k7bOpG1kBB5fnj8Ukv9N2kVdtW-AzfFQPBX2lM8Zta2eve0TFH9',
+                //'fDZEgjXDRSGLvnr3Cuxaai:APA91bGH9aLsy_vgOtKn7my0vuiOzg7ea_fMvAP8jfNBxNSM9oQMSdrr00HeFYn6XLkZl70URK9E_GdqwT9OKXkT8PQLH6J2cmWdJnxFYhx9jcatkDNf_R0S1U0CCwwSf5WLqayojL4M',
           },
         ),
       );
@@ -115,9 +174,6 @@ class HomeController extends GetxController {
     }
   }
 
-  Future<void> endAllCalls() async {
-    await FlutterCallkitIncoming.endAllCalls();
-  }
 }
 
 //  class HomeController extends GetxController {
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -6,32 +6,58 @@ import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-   HomeView({Key? key}) : super(key: key);
-  final controller =Get.put(HomeController());
+  HomeView({Key? key}) : super(key: key);
+  final controller = Get.put(HomeController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Video Call'),
         centerTitle: true,
+        actions: [
+          Obx(()=>
+            controller.isremoteUserchannelConnected.isTrue? Padding(
+              padding: const EdgeInsets.only(right: 15.0),
+              child: Text(
+                '${controller.currentTime}',
+                style: TextStyle(fontSize: 15),
+              ),
+            ):Text(''),
+          )
+        ],
       ),
-        body: SafeArea(
-          child:
-             Stack(
-              children: [
+      body: SafeArea(
+        child: Obx(
+          () => Stack(
+            children: [
+              if (!controller.isremoteUserConnected.value &&
+                  !controller.isremoteUserchannelConnected.value)
+                Center(
+                  child: Text(
+                    'Connecting...',
+                    style: TextStyle(fontSize: 15),
+                  ),
+                )
+              else if (!controller.isremoteUserchannelConnected.value)
+                Center(
+                  child: Text(
+                    'Ringing...',
+                    style: TextStyle(fontSize: 15),
+                  ),
+                )
+              else
                 AgoraVideoViewer(
                   client: controller.client,
-                  //showNumberOfUsers: true,
                   layoutType: Layout.oneToOne,
-                  //enableHostControls: true, // Add this to enable host controls
                 ),
-                  AgoraVideoButtons(
-                  client: controller.client,
-                  addScreenSharing: true, // Add this to enable screen sharing
-                ),
-              ],
-            ),
+              AgoraVideoButtons(
+                client: controller.client,
+                addScreenSharing: true, // Add this to enable screen sharing
+              ),
+            ],
           ),
+        ),
+      ),
     );
   }
 }
